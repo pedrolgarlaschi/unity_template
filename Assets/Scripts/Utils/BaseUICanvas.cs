d5ba21f96764fffa688eb98ef140eaaaf48e8582
P L Garlaschi Sousa <pedro@ps-macbook-pro.home>
2016-05-05 22:35:55 +0100

    add utils

---
+++ Assets/Scripts/Utils/BaseUICanvas.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace com.pedroleonardo
+{
+	/// <summary>
+	/// UI canvas event delegate
+	/// </summary>
+	public delegate void OnBaseUICanvasEvent(BaseUICanvas uiCanvas);
+
+	/// <summary>
+	/// Base class for all of the UI canvas, use to be an abstract class but i had some problems dispatching events from the Unity animtion
+	/// </summary>
+	public class BaseUICanvas : MonoBehaviour {
+
+
+		///Current canvas state
+		public enum CanvasState
+		{
+			opening,
+			open,
+			closing,
+			close
+		}
+
+		public OnBaseUICanvasEvent onUICanvasOpen;
+		public OnBaseUICanvasEvent onUICanvasClose;
+
+		[SerializeField]
+		protected string m_name;
+		public string Name
+		{
+			get{return m_name;}
+		}
+
+		protected bool m_isOpen = false;
+		public bool IsOpen
+		{
+			get{return m_isOpen;}
+		}
+
+		protected Canvas m_canvas;
+		public Canvas GetCanvas()
+		{
+			if(m_canvas == null)
+				m_canvas = gameObject.GetComponent<Canvas> ();
+
+			return m_canvas;
+		}
+
+		protected CanvasState m_state;
+		protected Animator m_animator;
+		protected Text[] m_textFields; 
+		protected System.Object m_params;
+		public CanvasScaler GetScaler()
+		{
+			return gameObject.GetComponent<CanvasScaler> ();
+		}
+
+
+		/// <summary>
+		/// I do not use the start or awake to init the canvas i call taht fundtion from the Navigation controller.
+		/// </summary>
+		public void Init()
+		{
+
+			m_animator = gameObject.GetComponent<Animator> ();
+			m_textFields = gameObject.GetComponentsInChildren<Text> ();
+			m_state = CanvasState.close;
+
+
+			if (m_animator != null)
+					m_animator.StopPlayback ();
+
+
+
+		}
+
+
+
+		/// <summary>
+		/// Open ui canvas
+		/// </summary>
+		/// <param name="param">Parameter.</param>
+		public virtual void Open(System.Object param)
+		{
+
+			m_state = CanvasState.opening;
+
+			if (m_animator != null)
+				m_animator.Play ("Intro");
+			else
+				OnAnimationIntroEnd ();
+
+		}
+			
+		/// <summary>
+		/// Close UI canvas
+		/// </summary>
+		public virtual void Close()
+		{
+			m_state = CanvasState.closing;
+
+
+			if (m_animator != null)
+				m_animator.Play ("Outro");
+			else
+				OnAnimationEndEnd ();
+		}
+
+		/// <summary>
+		/// Triggered when the animation intro ends
+		/// </summary>
+		public virtual void OnAnimationIntroEnd()
+		{
+			m_state = CanvasState.open;
+			m_isOpen = true;
+
+
+			onUICanvasOpen (this);
+		}
+
+		/// <summary>
+		/// Triggered when  the animation outro ends
+		/// </summary>
+		public virtual void OnAnimationEndEnd()
+		{
+
+			m_state = CanvasState.close;
+			m_isOpen = false;
+			onUICanvasClose (this);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/Utils/UINavigation.cs
@@ -0,0 +1,191 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace com.pedroleonardo
+{
+	public class UINavigation : MonoBehaviour {
+
+		private static UINavigation instance;
+		public static UINavigation Instance{
+			get{
+
+				if(instance == null){
+					
+					m_canvas = Resources.LoadAll<BaseUICanvas>("UI");
+				
+					m_listOfCanvas = new List<string>();
+
+					GameObject go = new GameObject();
+					
+					go.name = "ui_navigation";
+					instance = go.AddComponent<UINavigation>();
+
+					GameObject controllers =  GameObject.Find("controlers") as GameObject;
+
+					foreach(BaseUICanvas c in m_canvas)
+					{
+						if(Screen.width >= 2048)
+							c.GetScaler().matchWidthOrHeight = 1;
+						else
+							c.GetScaler().matchWidthOrHeight = 0;
+					}
+
+					
+					if(controllers != null)
+						go.transform.SetParent(controllers.transform);
+
+				}
+
+
+				return instance;
+			}
+			set{instance = value;}
+		}
+
+		private static BaseUICanvas[] m_canvas;
+		private static List<string> m_listOfCanvas;
+		private bool m_transition;	
+		private bool m_inTransition;
+
+		private BaseUICanvas m_nextCanvas;
+		private BaseUICanvas m_ref;
+		private System.Object m_nextCanvasParams;
+		private BaseUICanvas m_uiCanvas;
+		public BaseUICanvas MyUICanvas
+		{
+			get{return m_uiCanvas;}
+		}
+
+
+		void Awake()
+		{
+			if(!instance)
+				DontDestroyOnLoad(gameObject);
+		}
+
+
+		public string LastCanvasName
+		{
+			get{
+
+				if(m_listOfCanvas.Count > 1)
+					return m_listOfCanvas[m_listOfCanvas.Count - 2];
+				else
+					return "";
+			}
+		}
+
+		void OnLevelWasLoaded(int level) {
+
+			m_uiCanvas = null;
+			m_nextCanvas = null;
+		}
+
+		public BaseUICanvas OpenUICanvas<T>(string name , T param){
+			
+			if (m_uiCanvas != null && m_uiCanvas.Name == name)
+				return m_uiCanvas;
+
+			if (m_transition && m_ref != null && m_ref.Name == name && m_uiCanvas != null)
+				return m_uiCanvas;
+
+			if (m_transition){
+			
+				if(m_nextCanvas != null)
+				{
+					Destroy(m_nextCanvas.gameObject);
+					m_nextCanvas  = null;
+				}
+
+				if(m_uiCanvas != null)
+				{
+					Destroy(m_uiCanvas.gameObject);
+					m_uiCanvas = null;
+				}
+			}
+
+			m_transition = true;
+
+			m_ref = GetUICanvasRefByName (name);
+				
+			if (m_uiCanvas != null){
+				
+				m_nextCanvas = Instantiate (m_ref) as BaseUICanvas;
+				m_nextCanvas.GetCanvas().enabled = false;
+				m_nextCanvasParams = param;
+
+				m_uiCanvas.Close();
+
+
+				return m_nextCanvas;
+			}
+			else if(m_nextCanvas == null)
+				m_nextCanvas = Instantiate (m_ref) as BaseUICanvas;
+
+
+			m_uiCanvas = m_nextCanvas;
+			
+			m_uiCanvas.GetCanvas().enabled = true;
+
+			m_nextCanvasParams = null;
+
+			m_uiCanvas.Init ();
+			m_uiCanvas.onUICanvasClose = OnUICanvasClose;
+			m_uiCanvas.onUICanvasOpen = OnUICanvasOpen;
+
+
+			m_uiCanvas.name = m_ref.name;
+
+
+			m_listOfCanvas.Add (m_uiCanvas.Name);
+			m_uiCanvas.Open (param);
+
+			m_nextCanvas = null;
+
+			return m_uiCanvas;
+
+		}
+
+
+		public void CloseUICanvas(){
+			if (m_uiCanvas == null)
+				return;
+
+			m_transition = true;
+
+			m_uiCanvas.Close ();
+		}
+
+		private void OnUICanvasOpen(BaseUICanvas uiCanvas){
+			m_transition = false;
+		}
+		
+		private void OnUICanvasClose(BaseUICanvas uiCanvas){	
+
+			Destroy (m_uiCanvas.gameObject);
+			m_uiCanvas = null;
+
+			m_transition = false;
+
+			if (m_nextCanvas != null) {
+			
+				m_nextCanvas.GetCanvas().enabled = true;
+
+				OpenUICanvas (m_nextCanvas.Name, m_nextCanvasParams);
+			}
+		}
+
+		private BaseUICanvas GetUICanvasRefByName(string name){
+			foreach (BaseUICanvas c in m_canvas) 
+			{
+				if(c.Name == name)
+					return c;
+			}
+
+			return null;
+		}
+
+
+	}
+}
