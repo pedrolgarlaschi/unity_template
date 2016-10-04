5494edd7559af3e73052eff2be6709a9965a3442
P L Garlaschi Sousa <pedro.garlaschi@gmail.com>
2016-10-05 00:14:00 +0100

    tween

--- Assets/Scripts/Utils/SerialReader.cs
+++ Assets/Scripts/Utils/SerialReader.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using Uniduino;
 
 using System;
 using System.Threading;
 using System.IO.Ports;
+using System.IO;
 
 namespace com.pedroleonardo.serial
 {
@@ -44,7 +44,7 @@ namespace com.pedroleonardo.serial
 
 			m_listaners.Remove (listener);
 		}
-			
+
 		public bool SerialConnected
 		{
 			get{
@@ -63,11 +63,83 @@ namespace com.pedroleonardo.serial
 
 		void Start()
 		{
-			Connect (Uniduino.Arduino.guessPortName ());	
+			GetPortNames ();
+			Connect (guessPortName());
 		}
+
+		// Static Helpers	
+		private string guessPortName()
+		{		
+			switch (Application.platform)
+			{
+			case RuntimePlatform.OSXPlayer:
+			case RuntimePlatform.OSXEditor:
+			case RuntimePlatform.OSXDashboardPlayer:
+			case RuntimePlatform.LinuxPlayer:
+				return guessPortNameUnix();
+
+			default: 
+				return guessPortNameWindows();
+			}
+
+			//return guessPortNameUnix();
+		}
+
+		private string guessPortNameWindows()
+		{
+			var devices = System.IO.Ports.SerialPort.GetPortNames();
+
+			if (devices.Length == 0) // 
+			{
+				return "COM3"; // probably right 50% of the time		
+			} else
+				return devices[0];				
+		}
+
+		private string guessPortNameUnix()
+		{			
+			var devices = System.IO.Ports.SerialPort.GetPortNames();
+
+			if (devices.Length ==0) // try manual enumeration
+			{
+				devices = System.IO.Directory.GetFiles("/dev/");		
+			}
+			string dev = ""; ;			
+			foreach (var d in devices)
+			{				
+				if (d.StartsWith("/dev/tty.usb") || d.StartsWith("/dev/ttyUSB"))
+				{
+					dev = d;
+					//Debug.Log("Guessing that arduino is device " + dev);
+					break;
+				}
+			}		
+			return dev;		
+		}
+
+
+		private string[] GetPortNames ()
+		{
+			int p = (int)Environment.OSVersion.Platform;
+			List<string> serial_ports = new List<string> ();
+
+			// Are we on Unix?
+			if (p == 4 || p == 128 || p == 6) {
+				string[] ttys = Directory.GetFiles ("/dev/", "tty.*");
+				foreach (string dev in ttys) {
+					if (dev.StartsWith ("/dev/tty.*"))
+						serial_ports.Add (dev);
+					//Debug.Log (String.Format (dev));
+				}
+			}
+
+			return serial_ports.ToArray ();
+		}
+
 
 		public void Connect(string comName)
 		{
+
 			try
 			{
 				m_serial = new SerialPort (comName, 9600);
@@ -76,7 +148,7 @@ namespace com.pedroleonardo.serial
 				readThread.Start();
 			}
 			catch {
-			
+				Debug.Log ("error");
 			}
 		}
 
@@ -96,18 +168,23 @@ namespace com.pedroleonardo.serial
 			}
 		}
 
+		public void Disconnect()
+		{
+			m_serial.Close ();
+		}
+
+
 		void OnApplicationQuit() {
 
 			if(m_serial != null && m_serial.IsOpen)
 				m_serial.Close ();
 		}
 
-
 		void Update()
 		{
 			if (m_message != "")
 				BroadcastMessage ();
-				
+
 		}
 
 		private void BroadcastMessage()
@@ -125,4 +202,3 @@ namespace com.pedroleonardo.serial
 		void OnSerialMessage (string msg);
 	}
 }
-	
